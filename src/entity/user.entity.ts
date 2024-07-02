5c4c42779a62f55a0a90642dd7b07dbe818865e4
JangKroed <didlsdydgh@gmail.com>
2024-07-02 10:17:23 +0900

    .

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -3,6 +3,7 @@ import {
   CreateDateColumn,
   Entity,
   Generated,
+  JoinColumn,
   OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
@@ -93,7 +94,22 @@ export class UserModel {
   @Generated('uuid')
   additionalId: string;
 
-  @OneToOne(() => ProfileModel, (profile) => profile.user)
+  @OneToOne(() => ProfileModel, (profile) => profile.user, {
+    // find() 실행 할때마다 항상 같으 가져올 relation
+    eager: false,
+    // 저장할때 relation을 한번에 같이 저장 가능
+    cascade: true,
+    // null이 가능한지
+    nullable: false,
+    // 관계가 삭제됐을때
+    // no action -> 아무것도 안함
+    // cascade -> 참조하는 Row도 같이 삭제
+    // set null -> 참조하는 Row에서 참조 id를 null로 변경
+    // set default -> 기본 세팅으로 설정 (테이블의 기본 세팅)
+    // restrict -> 참조하고 있는 Row가 있는경우 참조당하는 Row 삭제 불가
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
   profile: ProfileModel;
 
   @OneToMany(() => PostModel, (post) => post.author)
